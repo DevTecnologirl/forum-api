01907a370c46400fe93b0e3cbd1d2c496f8bbe3f
camilly <tecnologirlinformatica@gmail.com>
2025-02-15 15:55:44 -0300

    feat: auth com JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Inject()
+    private readonly authService: AuthService;
+
+    @Post('singin')
+    singin(@Body() body: Prisma.UserCreateInput) {
+        return this.authService.singin(body);
+    }
+}
